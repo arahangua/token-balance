983597826ac263e57c4ba7f9444d46b769a129b1
arahangua <arahangua@gmail.com>
2024-08-30 13:07:02 +0000

    added: precomputed participant addresses, restructured the folder structure

--- config.py
+++ config.py
@@ -6,7 +6,8 @@ LIDO_TOKEN_ADDRESS = "0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84"
 
 # Block range for the pipeline
 START_BLOCK = 12000000  # Replace with your desired start block
-END_BLOCK = 12000100  # Replace with your desired end block
+END_BLOCK = 12010000  # Replace with your desired end block
+BLOCK_STEP_SIZE = 1000
 
 # Number of blocks to process in each batch
 BATCH_SIZE = 1000
@@ -15,6 +16,5 @@ BATCH_SIZE = 1000
 OUTPUT_FILE = "lido_token_balances.csv"
 
 # Pipeline configuration
-USE_PRECOMPUTED_LIST = False
+USE_PRECOMPUTED_LIST = False  #
 WEI_THRESHOLD = 1
-BLOCK_STEP_SIZE = 100  # Interval between blocks to collect data
